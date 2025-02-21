96147ae36b25dc903d3a2bd71c6d15e0e124d326
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-02-21 12:36:44 +1100

    user schema

--- models/coffee_model.js
+++ models/coffee_model.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 // Make a schema with data properties
 const CoffeeSchema = new mongoose.Schema({
-    name: {type:String, required:true},
+    name: { type: String, required: true },
     brand: String,
     type: String,
     description: String,
     cost: Number,
-    rating: {type:Number, required:true},
+    rating: { type: Number, required: true },
 });
 
 // Make a model that uses the schema
@@ -15,4 +15,3 @@ const CoffeeModel = mongoose.model("Coffee", CoffeeSchema);
 
 // Export the model
 module.exports = {};
-
