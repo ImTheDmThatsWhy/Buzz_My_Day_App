1797ba7a70035ba9710c4a5060491049976b5d8a
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-02-23 17:35:15 +1100

    Add coffee controller

--- controllers/coffee_controller.js
+++ controllers/coffee_controller.js
@@ -2,5 +2,33 @@ const Coffee = require("../models/coffee_model");
 
 async function getCoffees() {
     const coffees = await Coffee.find();
-    return categories;
+    return coffees;
 }
+
+async function getCoffee(coffeeId) {
+    const coffee = await Coffee.findbyId(coffeeId);
+    return coffee;
+}
+
+async function createCoffee(coffee) {
+    const newCoffee = await Coffee.create(coffee);
+    return newCoffee;
+}
+
+async function updateCoffee(CoffeeId, coffee) {
+    const updatedCoffee = await Category.findByIdandUpdate(coffeeId, coffee, {
+        new: true,
+    });
+    return updatedCoffee;
+}
+async function deleteCoffee(coffeeId) {
+    const deletedCoffee = await Coffee.findByIdAndDelete(coffeeID);
+    return deletedCoffee;
+}
+module.exports = {
+    getCoffees,
+    getCoffee,
+    createCoffee,
+    updateCoffee,
+    deleteCoffee,
+};
