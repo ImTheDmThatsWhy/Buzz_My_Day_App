cc662b3f96908a52ecf17d3c95111adb23ae22dc
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-02-24 11:48:10 +1100

    user controller created

---
+++ controllers/user_controller.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userLoginModel");
+
+async function registerUser(user) {
+    const existingUser = await User.findOne({ email: user.email });
+    if (existingUser) {
+        return { error: "Email already in use" };
+    }
+    //create a hashed password
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    //user creation
+    const UserCreated = await User.create({
+        name: user.name,
+        email: user.email,
+        password: hashedPassword,
+        is_admin: false,
+    });
+    //create jsonwebtoken
+    const payload = {
+        id: UserCreated._id,
+    };
+    const token = jwt.sign(payload, "confidential");
+    return { token: token, user_id: UserCreated._id };
+}
+async function loginUser(user) {
+    // check existence of user
+    const extinguisher = await User.findOne({ email: user.email });
+    if (!existingUser) {
+        return { error: "Incorrect email or password" };
+    }
+    //password match check
+    const isMatch = await bcrypt.compare(user.password, existingUser.password);
+    if (!isMatch) {
+        return { error: "Incorrect email or password" };
+    }
+    // jsw token creation
+    const payload = {
+        id: existingUser._id,
+    };
+    const token = jwt.sign(payload, "confidential");
+    return { token, user_id: existingUser._id };
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
