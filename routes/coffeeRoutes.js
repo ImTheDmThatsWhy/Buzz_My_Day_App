06e1b86c3a240e7930b34559f0cb76ef0bd091c6
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-02-25 16:07:51 +1100

    index file updated and coffee router created

--- routes/coffeeRoutes.js
+++ routes/coffeeRoutes.js
@@ -14,4 +14,34 @@ coffeeRouter.get("/", async (req, res) => {
     res.json(coffees);
 });
 
-coffeeRouter.get("/:coffeeId");
+coffeeRouter.get("/:coffeeId", async (req, res) => {
+    const coffee = await getCoffee(req.params.coffeeId);
+    if (coffee) {
+        res.json(coffee);
+    } else {
+        res.status(404).json({
+            error: `Category with id ${req.params.categoryId} not found`,
+        });
+    }
+});
+
+coffeeRouter.post("/", async (req, res) => {
+    const bodyData = {
+        title: req.body.title,
+        description: req.body.description,
+    };
+    const newCoffee = await createCoffee(bodyData);
+    res.status(201).json(newCoffee);
+});
+
+coffeeRouter.patch("/:coffeeId", async (req, res) => {
+    const deletedCoffee = await deleteCoffee(req.params.coffeeId);
+    if (deletedCoffee) {
+        res.json(deletedCoffee);
+    } else {
+        res.status(404).json({
+            error: `Coffee with id ${req.params.coffeeId} not found`,
+        });
+    }
+});
+module.exports = coffeeRouter;
