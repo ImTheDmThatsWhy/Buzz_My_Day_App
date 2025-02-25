0a6f8f8aa7646ff240fcb2c1aeb034b07f2df913
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-02-25 17:49:54 +1100

    changed model folder name to avoid clash with branch name

---
+++ model/accountModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    displayname: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: false
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Account', accountSchema);
---
+++ routes/postRoutes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const {
+    getPosts,
+    getPost,
+    createPost,
+    updatePost,
+    deletePost,
+} = require("../controllers/postControllers");
+
+const postRouter = express.Router();
+// Get all posts
+postRouter.get("/", async (req, res) => {
+    const posts = await getPosts();
+    res.json(posts);
+});
+
+// get a single post
+postRouter.get("/: postId", async (req, res) => {
+    const post = await getPost(req.params.postId);
+    if (post) {
+        res.json(post);
+    } else {
+        res.status(404).json({
+            error: `post with id ${req.params.postId} not found`,
+        });
+    }
+});
+
+// Post
+postRouter.post("/", async (req, res) => {
+    const bodyData = {
+        title: req.body.title,
+        body: req.body.body,
+        is_published: req.body.is_published,
+        post_id: req.body.post_id,
+        user_id: req.userId,
+    };
+    const newPost = await createPost(bodyData);
+    res.status(201).json(newPost);
+});
