853a1587ed4298867b123fdc0c4fa2bb695e9b64
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-02-26 10:44:28 +1100

    post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -33,7 +33,6 @@ postRouter.post("/", async (req, res) => {
         title: req.body.title,
         body: req.body.body,
         is_published: req.body.is_published,
-        post_id: req.body.post_id,
         user_id: req.userId,
     };
     const newPost = await createPost(bodyData);
@@ -46,5 +45,35 @@ postRouter.patch("/:postId", async (req, res) => {
         title: req.body.title,
         body: req.body.body,
         is_published: req.body.is_published,
+        post_id: req.body_post_id,
+        user_id: req.userId,
     };
+    const updatedPost = await updatePost(
+        req.params.postId,
+        bodyData,
+        req.userId
+    );
+    if (!updatedPost) {
+        res.status(404).json({
+            error: `Post with id ${req.params.postId} not found`,
+        });
+    } else if (updatedPost.error) {
+        res.status(403).json(updatedPost);
+    } else {
+        res.json(updatedPost);
+    }
 });
+
+// Delete posts/id
+postRouter.delete("/:postId", async (req, res) => {
+    const deletedPost = await deletePost(req.params.postId);
+    if (deletedPost) {
+        res.json(deletedPost);
+    } else {
+        res.status(404).json({
+            error: `Post with id ${req.params.postId}not found`,
+        });
+    }
+});
+
+exports = postRouter;
