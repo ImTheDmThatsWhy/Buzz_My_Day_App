948c3aea298114b86ba21a9ed43dd3c889ea0cf8
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-03-04 16:50:50 +1100

    fixed some errors

--- middlewares/checkIfAdmin.js
+++ middlewares/checkIfAdmin.js
@@ -1,25 +1,27 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const User = require("../models/user")
+const User = require("../models/userLoginModel");
 
 const checkIfAdmin = async (req, res, next) => {
-    let token = req.get("authorization") // Bearer the-actual-token
-    token = token?.split(" ")?.[1] // the-actual-token
+    let token = req.get("authorization"); // Bearer the-actual-token
+    token = token?.split(" ")?.[1]; // the-actual-token
     if (!token) {
-        return res.status(401).json({ error: "Unauthenticated" })
+        return res.status(401).json({ error: "Unauthenticated" });
     }
     try {
-        const payload = jwt.verify(token, "secret")
-        const user = await User.findById(payload.id)
+        const payload = jwt.verify(token, "secret");
+        const user = await User.findById(payload.id);
         if (!user.is_admin) {
-            throw new Error()
+            throw new Error();
         }
-        req.userId = payload.id
-        next()
-    } catch(err) {
-        console.log(err)
-        return res.status(401).json({ error: "Unauthenticated / not an admin" })
+        req.userId = payload.id;
+        next();
+    } catch (err) {
+        console.log(err);
+        return res
+            .status(401)
+            .json({ error: "Unauthenticated / not an admin" });
     }
-}
+};
 
-module.exports = checkIfAdmin
+module.exports = checkIfAdmin;
