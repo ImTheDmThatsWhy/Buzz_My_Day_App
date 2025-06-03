e0cdd2259247e88652859317ce235c7ba6d4fdb7
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-06-03 20:40:42 +1000

    fix fetching from render backend

--- coffee_front_end/src/FetchData.js
+++ coffee_front_end/src/FetchData.js
@@ -4,7 +4,7 @@ function FetchData() {
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
-    fetch('https://buzz-my-day-app-xaqh.onrender.com/coffee')
+    fetch('http://localhost:3000/coffee')
       .then((response) => response.json())
       .then((data) => setRecords({ data }))
       .catch((err) => console.log(err));
