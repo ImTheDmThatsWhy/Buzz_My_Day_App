68c9904e3858b9a4fe3118acea119f618fff6e38
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-06-07 12:10:48 +1000

    added backend test to upload

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,6 +3,7 @@ const config = {
   reporters: [
     ["github-actions", {silent: false}],
     "summary",
+    // Summary of Jest j unit gets inputted into the text-report.xml
     ["jest-junit", {
       outputDirectory: "./output",
       outputName: new Date().toUTCString().replaceAll(":", " ") + '-test-report.xml'
